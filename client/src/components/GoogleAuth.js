6ad9338c541398e3661fbf831b1e2b4d06e47ad6
Michael <michael.bretagne@gmail.com>
2019-01-31 22:22:31 -0800

    Add routing and google Auth

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "926830258941-75kkbsfaupblqug1ssbhgqkfirftul2l.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
