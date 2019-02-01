6d00fea07b73c6e5948aad79c081ec44902c2cec
Michael <michael.bretagne@gmail.com>
2019-02-01 14:27:11 -0800

    Add and wire up authentication reducer to redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+);
 serviceWorker.unregister();
